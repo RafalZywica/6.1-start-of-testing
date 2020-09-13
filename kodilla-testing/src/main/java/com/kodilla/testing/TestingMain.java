d06f95067c63c226660c823a7289f0cc93a809f3
Rafał Żywica <rafalzywica2701@wp.pl>
2020-09-14 01:44:27 +0200

    6.1 Added Calculator class and subsequent testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        Calculator calculator = new Calculator(2, 1);
+
+        int resultOfAddition = calculator.add();
+
+        if (resultOfAddition == 3) {
+            System.out.println("Test dodawania OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int resultOfSubtraction = calculator.subtract();
+
+        if (resultOfSubtraction == 1) {
+            System.out.println("Test odejmowania OK");
+        } else {
+            System.out.println("Error!");
+        }
+
     }
 }
