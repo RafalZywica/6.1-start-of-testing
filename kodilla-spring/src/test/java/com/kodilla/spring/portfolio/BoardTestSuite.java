eac237456e4f9a4f2dcaa1ac5b580d6f5d694bc9
Rafał Żywica <rafalzywica2701@wp.pl>
2020-12-29 20:12:33 +0100

    Added 14.2 Spring practice

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,61 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class BoardTestSuite {
+
+    @Test
+    void testDoneList() {
+        //Before
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
+        Board board = context.getBean(Board.class);
+        TaskList taskList = board.getDoneList();
+        //When
+        System.out.println("Done List: \n" + taskList);
+        //Then
+        //donothing
+    }
+    @Test
+    void testInProgressList() {
+        //Before
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
+        Board board = context.getBean(Board.class);
+        TaskList taskList = board.getInProgressList();
+        //When
+        System.out.println("In Progress List: \n" + taskList);
+        //Then
+        //donothing
+    }
+    @Test
+    void testToDoList() {
+        //Before
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
+        Board board = context.getBean(Board.class);
+        TaskList taskList = board.getToDoList();
+        //When
+        System.out.println("To do List \n " + taskList);
+        //Then
+        //donothing
+    }
+    @Test
+    void testAddTask() {
+        //Before
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
+        Board board = context.getBean(Board.class);
+        TaskList taskList = board.getDoneList();
+        //When
+        taskList.addTask("Do Laundry");
+        //Then
+        //donothing
+    }
+
+
+}
