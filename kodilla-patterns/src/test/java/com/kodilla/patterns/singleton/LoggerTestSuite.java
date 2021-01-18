dac1490654aaec887209ea8863976f8b0b0e8ba1
Rafał Żywica <rafalzywica2701@wp.pl>
2021-01-18 23:25:50 +0100

    Fixed multiple JUNIT versions

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -3,7 +3,7 @@ package com.kodilla.patterns.singleton;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoggerTestSuite {
 
