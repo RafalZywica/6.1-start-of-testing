b7b140577b30e51dfd99fde73ee641f3272ff23f
Rafał Żywica <rafalzywica2701@wp.pl>
2021-01-19 15:38:27 +0100

    Added 15.5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("Default")
+                .burgers(2)
+                .sauce("Spicy")
+                .ingredient("Tomato")
+                .ingredient("Lettuce")
+                .ingredient("Beacon")
+                .ingredient("Onion")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        assertEquals(4, howManyIngredients);
+    }
+}
